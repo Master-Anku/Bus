8494317d5e17d5e50137c05c4c67ff6eabfb4193
Ankura <Master-Anku.com>
2024-08-14 08:46:07 +0630

    Bus list and all

---
+++ flights.js
@@ -0,0 +1,82 @@
+const flightsfile = [
+    {
+        "departureIATA": "RGN",
+        "arrivalIATA": "BKK",
+        "departureTime": "09:00",
+        "departureDate": "Feb 01, 2024",
+        "arrivalDate": "Feb 01, 2024",
+        "arrivalTime": "10:00",
+        "status": "OK",
+        "duration": "1h 00m"
+    },
+    {
+        "departureIATA": "BKK",
+        "arrivalIATA": "RGN",
+        "departureTime": "13:00",
+        "departureDate": "Feb 01, 2024",
+        "arrivalDate": "Feb 01, 2024",
+        "arrivalTime": "14:00",
+        "status": "Delayed",
+        "duration": "1h 00m"
+    },
+    {
+        "departureIATA": "TPE",
+        "arrivalIATA": "JFK",
+        "departureTime": "06:00",
+        "departureDate": "Jan 03, 2024",
+        "arrivalDate": "Feb 03, 2024",
+        "arrivalTime": "09:00",
+        "status": "OK",
+        "duration": "17h 00m"
+    },
+    {
+        "departureIATA": "JFK",
+        "arrivalIATA": "TPE",
+        "departureTime": "13:00",
+        "departureDate": "Jan 01, 2024",
+        "arrivalTime": "06:00",
+        "arrivalDate": "Jan 01, 2024",
+        "status": "Delayed",
+        "duration": "17h 00m"
+    },
+    {
+        "departureIATA": "Yangon",
+        "arrivalIATA": "MDY",
+        "departureTime":"07:00am, 08:00am, 09:00am",
+        "departureDate": "Jan 03, 2024",
+        "arrivalDate": "jan 03, 2024",
+        "arrivalTime": "06:00, 07:00, 08:00",
+        "status": "17000ks Nomal , 35000ks VIP",
+        "duration": "11h 00m"
+    },
+    {
+        "departureIATA": "MDY",
+        "arrivalIATA": "Yangon",
+        "departureTime": "06:00",
+        "departureDate": "Jan 03, 2024",
+        "arrivalDate": "Feb 03, 2024",
+        "arrivalTime": "09:00",
+        "status": "OK",
+        "duration": "17h 00m"
+    },
+    {
+        "departureIATA": "Yangon",
+        "arrivalIATA": "BG",
+        "departureTime": "06:00",
+        "departureDate": "Jan 03, 2024",
+        "arrivalDate": "Feb 03, 2024",
+        "arrivalTime": "09:00",
+        "status": "OK",
+        "duration": "17h 00m"
+    },
+    {
+        "departureIATA": "BG",
+        "arrivalIATA": "Yangon",
+        "departureTime": "06:00",
+        "departureDate": "Jan 03, 2024",
+        "arrivalDate": "Feb 03, 2024",
+        "arrivalTime": "09:00",
+        "status": "OK",
+        "duration": "17h 00m"
+    },
+  ];
---
+++ script.js
@@ -0,0 +1,83 @@
+"use strict";
+
+// var Name = window.prompt("What is your name");
+// var Age = window.prompt("Your age?")
+
+// // Lo
+
+// var user = {
+//   Name: Name.trim(),
+//   Age: Age.trim(),
+
+// };
+
+// localStorage.setItem("user", JSON.stringify(user));
+
+// var storedUser = JSON.parse(localStorage.getItem("user"));
+// console.log(storedUser.Name); // 'Name' ကို ပြန်လည်ကြည့်နိုင်ပါတယ်
+// console.log(storedUser.Age);  // 'Age' ကို ပြန်လည်ကြည့်နိုင်ပါတယ်
+
+function htmlOption() {
+  let departure = document.querySelector("#departure");
+  let arrival = document.querySelector("#arrival");
+
+  // airports.forEach((ap) => {
+  //   let option = document.createElement("option");
+  //   option.value = ap.iata;
+  //   option.textContent = `${ap.name} (${ap.iata})`;
+  //   departure.appendChild(option);
+  //   arrival.appendChild(option.cloneNode(true));
+  
+  // });
+
+  airports.forEach((ap) =>{
+    let optionAp = `<option value="${ap.iata}" > ${ap.name} (${ap.iata})></option>`;
+    departure.insertAdjacentHTML("beforeend", optionAp);
+    arrival.insertAdjacentHTML("beforeend", optionAp);
+  })
+}
+
+function checkFlight() {
+  let departureIATA = document.querySelector("#departure").value.trim();
+  let arrivalIATA = document.querySelector("#arrival").value.trim();
+  let showResult = document.querySelector("#result");
+
+  // console.log('Departure IATA:', departureIATA);
+  // console.log('Arrival IATA:', arrivalIATA);
+
+  // Check if both departure and arrival IATA codes are selected
+  // if (departureIATA || arrivalIATA) {
+  //   showResult.textContent = `Select both departure and arrival airports!ဘာမှမရှိပါ ရွေးချယ်ပေးပါ`;
+  //   return;
+  // }
+
+  let flight = flightsfile.find((f) => f.departureIATA === departureIATA && f.arrivalIATA === arrivalIATA);
+
+  let departureAp = airports.find((a) => a.iata === departureIATA);
+  let arrivalAp = airports.find((a) => a.iata === arrivalIATA);
+
+  if (flight && departureAp && arrivalAp) {
+    showResult.innerHTML = `
+    <h2 class="resulth2">Flight Status</h2>
+    <p class="subTitle">Departure Airport ကားကွင်း: <span class="details">${departureAp.name} (${departureAp.iata})</span></p>
+    <p class="subTitle">Arrival Airport: <span class="details">${arrivalAp.name} (${arrivalAp.iata})</span></p>
+    <p class="subTitle">Departure Time: <span class="details">${flight.departureTime}</span></p>
+    <p class="subTitle">Arrival Time: <span class="details">${flight.arrivalTime}</span></p>
+    <p class="subTitle">Departure Date: <span class="details">${flight.departureDate}</span></p>
+    <p class="subTitle">Arrival Date: <span class="details">${flight.arrivalDate}</span></p>
+    <p class="subTitle">Status: <span class="details">${flight.status}</span></p>
+    <p class="subTitle">Duration: <span class="details">${flight.duration}</span></p>
+    `;
+  } 
+  else {
+    showResult.innerHTML = "Flight details NOT found!တစ်ခုခုကို ရွေးချယ်ပေးပါ";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  htmlOption();
+  // checkFlight();
+  document.querySelector("#checkStatus").addEventListener("click", checkFlight);
+});
+
+
